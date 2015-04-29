f925cfd0b0c2cb568a5ce747294356e5b123129b
Jack Hou <jack.hou@live.com>
2015-04-29 13:45:22 +1200

    Create Index Page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
 	before_action :find_article, only: [:show, :edit, :update, :destroy]
 
 	def index
+		@articles = Article.all.order("CREATED_AT DESC")
 	end
 
 	def new
